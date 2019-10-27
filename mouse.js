deed6a7fe9a8cd3d63e87702ec86ccb10049d994
DuctnNetwork <trannhuduc531998@gmail.com>
2019-10-27 20:50:06 +0700

    Add die method for mouse

--- mouse.js
+++ mouse.js
@@ -1,6 +1,12 @@
 function mouse(name) {
 	// body...
 	this.name = name;
+	this.dead = false;
 }
+
+mouse.prototype.die = function() {
+	// body...
+	this.dead = true;
+};
 
 module.exports = mouse;
