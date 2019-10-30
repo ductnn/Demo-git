06e437259d3b91d99810cce2252dd0b977ad67c4
DuctnNetwork <trannhuduc531998@gmail.com>
2019-10-30 10:28:43 +0700

    Add saySomething method for dog

--- Dog.js
+++ Dog.js
@@ -1,5 +1,8 @@
-function Dog() {
+var chalk = require('chalk'); 
+
+function Dog(name) {
 	// body...
+	this.name = name;
 	this.stomach = [];
 }
 
@@ -7,3 +10,10 @@ Dog.prototype.eat = function(Cat) {
 	// body...
 	this.stomach.push(Cat);
 };
+
+Dog.prototype.saySomething = function() {
+	// body...
+	console.log('Hello world' + chalk.blue(this.name));
+};
+
+module.exports = Dog;
